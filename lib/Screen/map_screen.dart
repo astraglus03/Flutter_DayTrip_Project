a4e5d792699ea659d93f32e579dfa455cb30a766
baejaemin <qqqaaaccc@naver.com>
2023-11-20 05:00:53 +0900

    두번째 탭 기본 ui 완성

--- lib/Screen/map_screen.dart
+++ lib/Screen/map_screen.dart
@@ -1,12 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
-  const MapScreen({super.key});
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("두번째 화면"),
-    );;
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Google Map
+          GoogleMap(
+            onMapCreated: (controller) {
+              setState(() {
+                mapController = controller;
+              });
+            },
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.7749, -122.4194),
+              zoom: 12.0,
+            ),
+          ),
+
+          // Floating Search Bar
+          Positioned(
+            top: 40.0,
+            left: 16.0,
+            right: 16.0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 5.0,
+                      spreadRadius: 2.0,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: '장소 검색...',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          // 검색 버튼이 클릭되었을 때 수행할 동작 추가
+                        },
+                        icon: Icon(Icons.search),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // Search Buttons
+          Positioned(
+            top: 120.0,
+            left: 16.0,
+            right: 16.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildSearchButton('버튼1'),
+                _buildSearchButton('버튼2'),
+                _buildSearchButton('버튼3'),
+                _buildSearchButton('버튼4'),
+              ],
+            ),
+          ),
+
+          // Draggable Scrollable Sheet
+          DraggableScrollableSheet(
+            initialChildSize: 0.2,
+            minChildSize: 0.1,
+            maxChildSize: 0.9,
+            builder: (context, scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 5.0,
+                      spreadRadius: 2.0,
+                    ),
+                  ],
+                ),
+                child: ListView.builder(
+                  controller: scrollController,
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Item $index'),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+
+          // Floating Button (위로 이동)
+          Positioned(
+            bottom: 16.0,
+            right: 16.0,
+            child: FloatingActionButton(
+              onPressed: () {
+                // 버튼이 클릭되었을 때 수행할 동작 추가
+              },
+              child: Icon(Icons.add),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchButton(String label) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white, // 배경 색을 하얀색으로 변경
+        borderRadius: BorderRadius.circular(30.0),
+        border: Border.all(color: Colors.blue, width: 1.0),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(30.0),
+          onTap: () {
+            // 각 버튼이 클릭되었을 때 수행할 동작 추가
+          },
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(color: Colors.blue),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
