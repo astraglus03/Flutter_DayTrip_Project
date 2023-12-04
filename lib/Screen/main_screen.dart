cffac1651c6ec3481837c3de1d7eebdcc7178cad
simjonghye <simjonghye@gmail.com>
2023-12-04 14:24:12 +0900

    홈스크린 전시 db 불러오기 성공, 요일별 불러오기 실패

--- lib/Screen/main_screen.dart
+++ lib/Screen/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:final_project/Screen/home_screen.dart';
 import 'package:final_project/Screen/map_screen.dart';
 import 'package:final_project/Screen/bookmark_screen.dart';
 import 'package:final_project/Screen/plus_widget.dart';
+import 'package:final_project/ThirdComponent/add_newExhibition.dart';
 import 'package:final_project/ThirdComponent/add_newspace.dart';
 import 'package:final_project/ThirdComponent/write_daylog.dart';
 import 'package:final_project/ThirdComponent/write_oneline.dart';
@@ -33,7 +34,6 @@ class _MainScreenState extends State<MainScreen> {
 
     // 바텀내비바 카메라
     MyPageScreen(),
-
   ];
 
   void _onItemTapped(int index) {
@@ -74,7 +74,7 @@ class _MainScreenState extends State<MainScreen> {
                     borderRadius: BorderRadius.circular(50),
                     color: Colors.grey[300],
                   ),
-                  child: Icon(Icons.add_location_rounded, color: Colors.red,),
+                  child: Icon(Icons.short_text, color: Colors.deepPurple,),
                 ),
                 title: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -133,7 +133,7 @@ class _MainScreenState extends State<MainScreen> {
                     borderRadius: BorderRadius.circular(50),
                     color: Colors.grey[300],
                   ),
-                  child: Icon(Icons.place, color: Colors.white,),
+                  child: Icon(Icons.place, color: Colors.red,),
                 ),
                 title: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -153,6 +153,37 @@ class _MainScreenState extends State<MainScreen> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => AddNewSpace()),
+                  );
+                },
+              ),
+              SizedBox(height: 10,),
+              ListTile(
+                leading: Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[300],
+                  ),
+                  child: Icon(Icons.museum, color: Colors.green,),
+                ),
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("전시∙행사 추가", style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                    ),),
+                    SizedBox(height: 3,),
+                    Text("새로운 전시∙행사를 추가해 보세요.", style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 14,
+                    ),),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AddNewExhibition()),
                   );
                 },
               ),
--- lib/model_db/space.dart
+++ lib/model_db/space.dart
@@ -1,6 +1,4 @@
-// 공간 추가 모델
-
-import 'dart:ui';
+import 'package:intl/intl.dart';
 
 class SpaceModel {
   final String spaceName;
@@ -8,7 +6,10 @@ class SpaceModel {
   final String locationName;
   final String tag;
   final String image;
-
+  //final String? recom_tag; // 추가된 부분
+  final String? exhibi_tag; // 추가된 부분
+  final DateTime? exhibi_date; // 추가된 부분
+  final String? exhibi_name; // 추가된 부분
 
   SpaceModel({
     required this.spaceName,
@@ -16,33 +17,47 @@ class SpaceModel {
     required this.tag,
     required this.image,
     required this.locationName,
+    //this.recom_tag,
+    this.exhibi_tag,
+    this.exhibi_date,
+    this.exhibi_name,
   });
 
-  SpaceModel.fromJson({ // ➊ JSON으로부터 모델을 만들어내는 생성자
-    required Map<String, dynamic> json,
-  })  : spaceName = json['spaceName'],
+  SpaceModel.fromJson(Map<String, dynamic> json)
+      : spaceName = json['spaceName'],
         location = json['location'],
         tag = json['tag'],
         image = json['image'],
-        locationName = json['locationName'];
+        locationName = json['locationName'],
+        //recom_tag = json['recom_tag'], // 업데이트된 부분
+        exhibi_tag = json['exhibi_tag'], // 업데이트된 부분
+        exhibi_date = DateFormat('yyyy년 MM월 dd일').parse(json['exhibi_date']),
+        exhibi_name = json['exhibi_name']; // 업데이트된 부분
 
-  Map<String, dynamic> toJson() {  // ➋ 모델을 다시 JSON으로 변환하는 함수
+  Map<String, dynamic> toJson() {
     return {
       'spaceName': spaceName,
       'location': location,
       'tag': tag,
       'image': image,
       'locationName': locationName,
+      //'recom_tag': recom_tag, // 업데이트된 부분
+      'exhibi_tag': exhibi_tag, // 업데이트된 부분
+      'exhibi_date': DateFormat('yyyy년 MM월 dd일').format(exhibi_date!), // 업데이트된 부분
+      'exhibi_name': exhibi_name, // 업데이트된 부분
     };
   }
 
-  SpaceModel copyWith({  // ➌ 현재 모델을 특정 속성만 변환해서 새로 생성
+  SpaceModel copyWith({
     String? spaceName,
     String? location,
     String? tag,
     String? image,
     String? locationName,
-    String? recom_tag,
+    //String? recom_tag,
+    String? exhibi_tag,
+    DateTime? exhibi_date,
+    String? exhibi_name,
   }) {
     return SpaceModel(
       spaceName: spaceName ?? this.spaceName,
@@ -50,8 +65,9 @@ class SpaceModel {
       tag: tag ?? this.tag,
       image: image ?? this.image,
       locationName: locationName ?? this.locationName,
+      exhibi_tag: exhibi_tag ?? this.exhibi_tag, // 업데이트된 부분
+      exhibi_date: exhibi_date ?? this.exhibi_date, // 업데이트된 부분
+      exhibi_name: exhibi_name ?? this.exhibi_name, // 업데이트된 부분
     );
   }
-
-
 }
